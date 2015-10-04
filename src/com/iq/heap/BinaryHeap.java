b4b16185ff4dbb0b2444f9499f2959c942f299f4
joyghosh <joy.ghosh@tiatros.com>
2015-10-04 16:12:26 +0530

    joyghosh: array

---
+++ src/com/iq/heap/BinaryHeap.java
@@ -0,0 +1,16 @@
+package com.iq.heap;
+
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class BinaryHeap {
+	
+	private static final int d = 2;
+	private int heapSize;
+	private int[] heap;
+	
+	public BinaryHeap(int capacity){
+		
+	}
+}
---
+++ src/com/joy/array/MaxSumSubArray.java
@@ -0,0 +1,53 @@
+package com.joy.array;
+
+//Program to find the max. sum of a contiguous sub-array using Kadane's algorithm. 
+public class MaxSumSubArray {
+	
+	public static void main(String []args){
+		int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+		System.out.println("Max. sum of contiguous sub-array.");
+		System.out.println(maxSumSubArray(arr));
+	}
+	
+	//find the max. sum of the contiguous sub-array.
+	private static int maxSumSubArray(int[] arr){
+		int sum = 0;
+		
+		if(isAllNegative(arr)){
+			
+			sum = arr[0];
+			for(int i=1;i<arr.length;i++){
+				if(arr[i]>sum)
+					sum = arr[i];
+			}
+		}else{
+			
+			int max_so_far, max_ending_here;
+			max_so_far = max_ending_here = 0;
+			
+			for(int i=0;i<arr.length;i++){
+				max_ending_here += arr[i];
+				if(max_ending_here < 0)
+						max_ending_here = 0;
+				else if(max_so_far < max_ending_here)
+					max_so_far = max_ending_here;
+			}
+			
+			sum = max_so_far;
+		}
+		return sum;
+	}
+	
+	//Check for all negative array.
+	private static boolean isAllNegative(int[] arr){
+		
+		boolean flag = true;
+		for(int i=0;i<arr.length;i++){
+			if(!(arr[i]<0)){
+				flag = false;
+				break;
+			}
+		}
+		return flag;
+	}
+}
