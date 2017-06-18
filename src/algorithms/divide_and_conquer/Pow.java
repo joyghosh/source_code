9224f842d4f376631782bf6978baacdde0c4c8db
Joy Ghosh <joyghosh@Joys-MacBook-Pro.local>
2017-06-18 22:23:55 +0530

    pow function log n

--- src/algorithms/divide_and_conquer/Pow.java
+++ src/algorithms/divide_and_conquer/Pow.java
@@ -1,5 +1,7 @@
 package algorithms.divide_and_conquer;
 
+import java.util.Scanner;
+
 /**
  * Power function using divide and conquer.
  * @author Joy Ghosh
@@ -8,8 +10,27 @@ package algorithms.divide_and_conquer;
 public class Pow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x: ");
+		int x = sc.nextInt();
+		System.out.println("Enter y: ");
+		int y = sc.nextInt();
+		System.out.println("Power result "+power(x, y));
+		sc.close();
 	}
-
+	
+	//Time complexity O(log y)
+	private static long power(int x, int y){
+		if(x==1) return x;
+		if(y==0) return 1;
+		
+		long result;
+		if(y%2==0){
+			result = power(x,y/2);
+			return (result*result);
+		}
+		
+		result = power(x, (y-1)/2);
+		return x*result*result;
+	}
 }
